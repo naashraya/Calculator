dc62354e39c7d9fb8ff5080217f08c3239d089e2
aashraya <naashraya@gmail.com>
2025-01-08 23:05:29 +0545

    file contents added

--- calc.js
+++ calc.js
@@ -1,8 +1,26 @@
 const display = document.querySelector('.display'); 
 const controlButtons = document.querySelector('.control').children; 
+const allSymbols = ['+', '-', 'X', '÷', '=', '%', '(', ')', 'C' ];
+
+let firstValue = '';
+let secondValue = '';
+let symbol = '';
+
+const calculate = () =>{
+    firstValue = parseFloat(firstValue);
+}
 
 for (let button of controlButtons) {
     button.addEventListener('click', () => {
-        display.innerText = button.innerText; 
+        const {innerText: btnValue} = button;
+        const btnValueIsSymbol = allSymbols.includes(btnValue);
+
+        if(firstValue && btnValueIsSymbol){
+            symbol = btnValue;
+        }
+        else if (!symbol) firstValue += btnValue;
+        else if (symbol)secondValue += btnValue;
+
+        if (btnValue != '=' && btnValue != 'C') display.innerText += btnValue; 
     });
 }
